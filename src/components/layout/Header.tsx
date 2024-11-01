e8eb3c2dfae2777d1defaaa09a5c1b6838c2524a
Jon Howard <johnny.rtn@gmail.com>
2024-11-01 13:14:53 -0230

    add logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,16 +2,19 @@
 
 import { useSearch } from "@/contexts/SearchContext";
 import { Search } from "@/components/Search";
+import Image from "next/image";
+
+import logo from "./assets/logo.webp";
 
 export default function Header() {
   const { setSearchTerm } = useSearch();
 
   return (
-    <header className="bg-gray-900 text-white py-4 px-8 flex items-center justify-between">
-      {/* Logo */}
-      <div className="text-xl font-bold">Logo</div>
+    <header className="py-4 px-8 flex items-center justify-between">
+      <div className="relative">
+        <Image src={logo} alt="Reddit!" width={100} />
+      </div>
 
-      {/* Search Bar */}
       <div className="flex-1 mx-4">
         <Search onSearch={setSearchTerm} />
       </div>
