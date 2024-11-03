edb4fd07a40ece0ceb6223c30876759ae2a51a23
Jon Howard <johnny.rtn@gmail.com>
2024-11-03 08:50:51 -0330

    add icons, implement sheet and dropdown menu components

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,28 @@ import { useSearch } from "@/contexts/SearchContext";
 import { Search } from "@/components/Search";
 import Image from "next/image";
 
-import logo from "./assets/logo.webp";
+import redditLogo from "./assets/logo.webp";
+import redditIcon from "./assets/reddit-icon.webp";
+import menuIcon from "./assets/menu.svg";
+import userIcon from "./assets/user-icon.svg";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Header() {
   const { setSearchTerm } = useSearch();
@@ -12,16 +33,59 @@ export default function Header() {
   return (
     <header className="py-4 px-8 flex items-center justify-between">
       <div className="relative">
-        <Image src={logo} alt="Reddit!" width={100} />
+        <div className="flex">
+          <Sheet>
+            <SheetTrigger>
+              <Image
+                className="md:hidden mr-4"
+                src={menuIcon}
+                alt="Reddit!"
+                width={35}
+              />
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                <SheetDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+          <Image
+            className="md:hidden"
+            src={redditIcon}
+            alt="Reddit!"
+            width={35}
+          />
+          <Image
+            className="hidden md:block"
+            src={redditLogo}
+            alt="Reddit!"
+            width={100}
+          />
+        </div>
       </div>
 
       <div className="flex-1 mx-4 max-w-lg">
         <Search onSearch={setSearchTerm} />
       </div>
 
-      {/* User Icon */}
-      <div className="text-white">
-        <span className="material-icons">account_circle</span>
+      <div>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <img src={userIcon.src} width={35} alt="" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </header>
   );
