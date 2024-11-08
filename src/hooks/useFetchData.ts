e582ae59182b74658d3ad88f033f1100e39607d2
Jon Howard <johnny.rtn@gmail.com>
2024-11-08 11:15:06 -0330

    refactor to combine results

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -1,20 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import { FetchState } from "@/types/formTypes";
 
-function useFetchData<T>(url: string): FetchState<T> {
+function useFetchData<T>(url: string): FetchState<T[]> {
   const { data, error, isLoading } = useQuery({
     queryKey: ["fetchData", url],
-    queryFn: async (): Promise<T> => {
+    queryFn: async (): Promise<T[]> => {
       const response = await fetch(url);
       if (!response.ok) throw new Error("Network response was not ok");
-      return response.json() as Promise<T>;
+      const jsonData = await response.json();
+      // Assuming jsonData has the structure { data: { children: [{ data: T }] } }
+      return jsonData.data.children.map((child: { data: T }) => child.data);
     },
     refetchOnWindowFocus: false,
     retry: 1,
   });
 
   return {
-    data: data ?? null, // Ensure data is T | null
+    data: data ?? null, // Ensure data is T[] | null
     error: error ? error.message : null,
     isLoading,
   };
